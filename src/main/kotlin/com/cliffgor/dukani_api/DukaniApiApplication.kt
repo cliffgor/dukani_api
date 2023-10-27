6c9ccbfec6ce852eeb57df61382c9d3c4c39b981
cliffgor <clierozigy@gmail.com>
2023-10-27 21:15:54 +0300

    Added springboot and API resources

---
+++ src/main/kotlin/com/cliffgor/dukani_api/DukaniApiApplication.kt
@@ -0,0 +1,11 @@
+package com.cliffgor.dukani_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DukaniApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<DukaniApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/cliffgor/dukani_api/DukaniApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cliffgor.dukani_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DukaniApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
