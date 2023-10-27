21b28b6175b965416e766b12fb59ab8712901a38
cliffgor <clierozigy@gmail.com>
2023-10-27 22:41:46 +0300

    Add the user entity which holds details about the user

---
+++ src/main/kotlin/com/cliffgor/dukani_api/models/User.kt
@@ -0,0 +1,24 @@
+package com.cliffgor.dukani_api.models
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Int=0
+
+    @Column
+    var name= ""
+
+    @Column(unique = true)
+    var email=""
+
+    @Column
+    var password = ""
+
+}
