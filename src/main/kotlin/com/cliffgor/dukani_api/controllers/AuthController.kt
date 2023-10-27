7a1405201d1dd79c17012a9017fb070f44dcf508
cliffgor <clierozigy@gmail.com>
2023-10-27 22:57:04 +0300

    Add prefix api/ to connect with all endpoints

--- src/main/kotlin/com/cliffgor/dukani_api/controllers/AuthController.kt
+++ src/main/kotlin/com/cliffgor/dukani_api/controllers/AuthController.kt
@@ -1,12 +1,14 @@
 package com.cliffgor.dukani_api.controllers
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
+@RequestMapping("api")
 class AuthController {
-    @GetMapping("register")
+    @GetMapping("api/register")
     fun register(): String {
         return "register your credentials"
     }
